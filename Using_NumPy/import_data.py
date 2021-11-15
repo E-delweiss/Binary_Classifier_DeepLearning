7f10d8594bd98a49fd80bb115fe1dc9c4b3ed9ce
ThOpaque <thierry.kss@posteo.net>
2021-11-15 22:34:03 +0100

    Updates

--- Using_NumPy/import_data.py
+++ Using_NumPy/import_data.py
@@ -7,8 +7,8 @@ Created on Wed Oct 27 15:29:52 2021
 """
 
 import numpy as np
-import glob
 import math
+import glob
 from PIL import Image
 
 
@@ -86,50 +86,53 @@ def load_data(val_size=0.2):
     return data_train, label_train, data_val, label_val, classes
 
 
-def random_mini_batches(X, Y, mini_batch_size = 64, seed = 0):
+def random_mini_batches(X, Y, mini_batch_size = 64):
     """
     Creates a list of random minibatches from (X, Y)
     
-    Arguments:
-    X -- input data, of shape (input size, number of examples)
-    Y -- true "label" vector (1 for blue dot / 0 for red dot), of shape (1, number of examples)
-    mini_batch_size -- size of the mini-batches, integer
+    Parameters
+    ----------
+    X : np.array
+        Input data, of shape (input size, number of examples)
+    Y : np.array
+        True "label" vector, of shape (1, number of examples)
+    mini_batch_size : int
+        Size of the mini-batches
     
-    Returns:
-    mini_batches -- list of synchronous (mini_batch_X, mini_batch_Y)
+    Returns
+    -------
+    mini_batches : list
+        List of synchronous (mini_batch_X, mini_batch_Y)
     """
-    
-    np.random.seed(seed)            # To make your "random" minibatches the same as ours
-    m = X.shape[1]                  # number of training examples
+    m = X.shape[1]
     mini_batches = []
-        
-    # Step 1: Shuffle (X, Y)
+    
+    # Shuffling X, Y
     permutation = list(np.random.permutation(m))
     shuffled_X = X[:, permutation]
     shuffled_Y = Y[:, permutation].reshape((1, m))
     
-    inc = mini_batch_size
-
-    # Step 2 - Partition (shuffled_X, shuffled_Y).
-    # Cases with a complete mini batch size only i.e each of 64 examples.
-    num_complete_minibatches = math.floor(m / mini_batch_size) # number of mini batches of size mini_batch_size in your partitionning
+    num_complete_minibatches = math.floor(m / mini_batch_size)
     for k in range(0, num_complete_minibatches):
-        mini_batch_X = shuffled_X[:, k * inc : (k+1) * inc]
-        mini_batch_Y = shuffled_Y[:, k * inc : (k+1) * inc]
-    
+        start_minibatch = k * mini_batch_size
+        end_minibatch = (k+1) * mini_batch_size
+        mini_batch_X = shuffled_X[:, start_minibatch : end_minibatch]
+        mini_batch_Y = shuffled_Y[:, start_minibatch : end_minibatch]
+        
         mini_batch = (mini_batch_X, mini_batch_Y)
         mini_batches.append(mini_batch)
-    
-    # For handling the end case (last mini-batch < mini_batch_size i.e less than 64)
-    if m % mini_batch_size != 0:
-        mini_batch_X = shuffled_X[:, (k+1) * inc : ]
-        mini_batch_Y = shuffled_Y[:, (k+1) * inc : ]
         
+    if m % mini_batch_size != 0:
+        mini_batch_X = shuffled_X[:, end_minibatch : ]
+        mini_batch_Y = shuffled_Y[:, end_minibatch : ]
+    
         mini_batch = (mini_batch_X, mini_batch_Y)
         mini_batches.append(mini_batch)
     
     return mini_batches
-
+    
+    
+    
 
 if False :
     data_train, label_train, data_val, label_val, classes = load_data()
@@ -140,4 +143,23 @@ if False :
     img.show()
     
     print ("y = " + str(label_train[INDEX]) + ". It's a " + classes[label_train[INDEX].item()].decode("utf-8") +  " picture.")
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
